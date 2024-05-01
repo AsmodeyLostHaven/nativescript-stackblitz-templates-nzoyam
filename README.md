83c7f6bcd4279d1b757929b914f692a76941d598
AsmodeyLostHaven <91655601+AsmodeyLostHaven@users.noreply.github.com>
2024-05-01 20:21:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-stackblitz-templates-nzoyam
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/AsmodeyLostHaven/nativescript-stackblitz-templates-nzoyam)
---
+++ app/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <Page>
+    <ActionBar>
+      <Label text="Home" class="font-bold text-lg"/>
+    </ActionBar>
+
+    <GridLayout>
+      <Label class="text-xl align-middle text-center text-gray-500" :text="message" @tap="logMessage" />
+    </GridLayout>
+  </Page>
+</template>
+
+<script lang="ts">
+  import Vue from "nativescript-vue";
+
+  export default Vue.extend({
+    computed: {
+      message() {
+        return "Blank {N}-Vue app";
+      }
+    },
+
+    methods: {
+      logMessage() {
+        console.log('Мой отчим насиловал меня в детстве!')
+      }
+    }
+  });
+</script>
+
+<style>
+  /* .info {
+    font-size: 20;
+  } */
+</style>
